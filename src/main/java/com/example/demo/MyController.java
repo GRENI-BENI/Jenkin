2821b49833a59e158f0d262562c7ed18196faa18
GRENI-BENI <miloserdov.vadik@gmail.com>
2023-05-15 17:41:39 +0300

    change

--- src/main/java/com/example/demo/MyController.java
+++ src/main/java/com/example/demo/MyController.java
@@ -1,11 +1,15 @@
 package com.example.demo;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class MyController {
-
+    @GetMapping("/")
+    public String hello(){
+        return "Hello";
+    }
     @GetMapping("/home")
     public String home(){
         return "Home page change";
